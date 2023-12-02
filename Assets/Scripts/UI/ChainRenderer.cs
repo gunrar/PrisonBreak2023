5ca21287055734d4033a29e4b07b349405c24436
Gunnar <gunnartresch@gmail.com>
2023-12-01 19:04:21 -0800

    fix

--- Assets/Scripts/UI/ChainRenderer.cs
+++ Assets/Scripts/UI/ChainRenderer.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class ChainRenderer : MonoBehaviour
 {
-    public GameObject startObject; // Assign in inspector or via code
-    public GameObject endObject;   // Assign in inspector or via code
+    //public GameObject startObject; // Assign in inspector or via code
+    //public GameObject endObject;   // Assign in inspector or via code
 
-    private LineRenderer lineRenderer;
+    //private LineRenderer lineRenderer;
 
-    void Start()
-    {
-        // Get the LineRenderer component
-        lineRenderer = GetComponent<LineRenderer>();
+    //void Start()
+    //{
+    //     Get the LineRenderer component
+    //    lineRenderer = GetComponent<LineRenderer>();
 
-        // Optional: Configure the appearance of the line
-        lineRenderer.startWidth = 0.05f;
-        lineRenderer.endWidth = 0.05f;
-        lineRenderer.positionCount = 2;
-    }
+    //     Optional: Configure the appearance of the line
+    //    lineRenderer.startWidth = 0.05f;
+    //    lineRenderer.endWidth = 0.05f;
+    //    lineRenderer.positionCount = 2;
+    //}
 
-    void Update()
-    {
-        startObject = transform.gameObject;
-        endObject = GetComponent<PlayerControllerNetworked>().otherPlayer;
-        if (startObject != null && endObject != null)
-        {
-            // Update the positions of the line to match the GameObjects
-            lineRenderer.SetPosition(0, startObject.transform.position);
-            lineRenderer.SetPosition(1, endObject.transform.position);
-        }
-    }
+    //void Update()
+    //{
+    //    startObject = transform.gameObject;
+    //    endObject = GetComponent<PlayerControllerNetworked>().otherPlayer;
+    //    if (startObject != null && endObject != null)
+    //    {
+    //         Update the positions of the line to match the GameObjects
+    //        lineRenderer.SetPosition(0, startObject.transform.position);
+    //        lineRenderer.SetPosition(1, endObject.transform.position);
+    //    }
+    //}
 }
